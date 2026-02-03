fe667c479cf48642c902a7a57e2935010c1bfb95
madisonkllorente <madisonkllorente@students.abtech.edu>
2026-02-03 18:11:55 -0500

    Add files via upload

---
+++ CSC121-3.3YOListMKL.py
@@ -0,0 +1,8 @@
+#MKL CSC121 3.4 Your Own List
+
+subaru = ['Outback', 'Crosstrek', 'BRZ', 'Baja', 'Forester', 'Tribeca']
+
+listmessage = "I want these Subaru models: "
+print (f"I want these Subaru models: {', '.join(subaru)}")
+print (f"I own a {subaru[1]}")
+print (f"My childhood car was {subaru[5]}")
