d75c74fa30168e85a6e2cc5f45ccf165cdfe145e
madisonkllorente <madisonkllorente@students.abtech.edu>
2026-02-17 14:24:53 -0500

    Add files via upload

---
+++ 5.3AliensIteration1MKL.py
@@ -0,0 +1,11 @@
+#5.3 Aliens iterations MKL
+import random
+
+alien_color=['green','yellow','red']
+
+##random.shuffle(alien_color)
+
+for color in alien_color:
+    if color == 'green':
+        print("Player earned 5 points")
+    break
