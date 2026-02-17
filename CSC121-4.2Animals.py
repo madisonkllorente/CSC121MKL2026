e0d25b7f4bab496c5793f687e18831ecd8d872fa
madisonkllorente <madisonkllorente@students.abtech.edu>
2026-02-17 00:50:04 -0500

    Add files via upload

---
+++ CSC121-4.2Animals.py
@@ -0,0 +1,8 @@
+#4.2 Animals MKL
+
+animals = ['Geckos', 'Parrots', 'Praying Mantises']
+
+for animal in (animals):
+    print (f"{animal.title()} are fun animals to find in the wild!")
+print("\n They are cool and commonly green species!")
+
