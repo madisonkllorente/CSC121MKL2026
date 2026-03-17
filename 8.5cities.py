742c1357dd995ba7aa1ab61b1d8867a074a9b8f9
madisonkllorente <madisonkllorente@students.abtech.edu>
2026-03-17 18:29:47 -0400

    Add files via upload

---
+++ 8.5cities.py
@@ -0,0 +1,8 @@
+#8.5 Cities MKL
+
+def describe_city(city, country="USA"):
+    print(f"{city} is in the {country}.")
+
+describe_city("Asheville")          
+describe_city("Paris","France")          
+describe_city("Santo Domingo", "Dominican Republic") 
